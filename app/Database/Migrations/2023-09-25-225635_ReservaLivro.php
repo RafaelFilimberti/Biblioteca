9de782051acce7b489940065662bdc8b416aebb5
JAQUELINE KOGH MOTA <jaqueline.mota@alunos.sc.senac.br>
2023-09-25 20:35:56 -0300

    criei um nova migration para reserva_livro

---
+++ app/Database/Migrations/2023-09-25-225635_ReservaLivro.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ReservaLivro extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+
+            'id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'id_reserva' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
+            'id_livro' => ['type' => 'int', 'constraint' => 11],
+            'created_at'        => ['type' => 'datetime', 'null' => true],
+            'updated_at'        => ['type' => 'datetime', 'null' => true],
+            'deleted_at'        => ['type' => 'datetime', 'null' => true],
+        ]);
+
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('reservalivro');
+        $this->forge->addForeignKey('id_reserva', 'reservas', 'id');
+        $this->forge->addForeignKey('id_livro', 'livros', 'id');
+    }
+
+
+    public function down()
+    {
+        //
+    }
+}
+
+   
