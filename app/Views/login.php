c5fb4b0ca1e2fc0d294a74dc5d7003f88f441baf
RAFAEL FILIMBERTI <rafael.filimberti@alunos.sc.senac.br>
2023-11-13 21:50:36 -0300

    alteração front end

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Tela de Login</title>
+  <title>Tela de Login</title>
 </head>
 <!DOCTYPE html> <!--  -->
 
@@ -12,21 +13,26 @@
   <meta charset="UTF-8">
   <meta name="keywords" content="HTML, CSS, JavaScript">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> 
-  <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css") ?>"> 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" inegtrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/login.css") ?>">
 </head>
+
 <body>
-    <h1>Tela de Login</h1>
-    <form method="POST" action="">
-        <label for="email">Usuário:</label>
-        <input type="text" id="email" name="email" required><br>
 
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br>
+  
+  <form method="POST" action="">
+    <h1>Login</h1>
 
-        <input type="submit" value="Login">
-    </form>
+    <label for="email">Usuário:</label>
+    <input type="text" id="email" name="email" required><br>
+
+    <label for="senha">Senha:</label>
+    <input type="password" id="senha" name="senha" required><br>
+
+    <input type="submit" value="Login">
+  </form>
 </body>
+
 </html>
